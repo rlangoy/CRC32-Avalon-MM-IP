1fe9ade40e0eb7454858b4b350388786d69318b5
Rune Langøy <rune.langoy@gmail.com>
2020-11-18 10:47:06 +0100

    Code clean up
    Created function for Reset,Read and Write for added readability

--- ip/CRC32/avs_crc32_wraper_tb.sv
+++ ip/CRC32/avs_crc32_wraper_tb.sv
@@ -22,92 +22,104 @@ module test;
   
 
   initial begin 
+      //Initial/Statup Values
       read  = 0;
       clk   = 0; 
       reset = 1;
       write=0;
       address =8'h00;
-      #1
-      reset = 1; 
-      #1
-      reset = 0; 
-      read  = 1;  
-      #1
-      read  = 0;    
-      address =8'h00;
-      writedata= 8'h01;
-      write=1;
-      #1
-      write=0;
-      address =8'h00;
-      read  = 1;  
-    
-      #1
-      read  = 0;    
-      writedata= 8'h01;
-      write=1;
-      address =8'b0;
 
       #1
-      write=0;
-      read  = 1;  
+      hwReset();
+    
     
       #1
-      read  = 0;    
-      writedata= 8'h01;
-      write=1;
-      address =8'h01;
+      write2Address(8'h00,8'h01);
 
       #1
-      write=0;
-      read  = 1;  
-      address =8'h00;
+      readAddress(8'h00);
+
+      #1
+      write2Address(8'h00,8'h01);
+
+      #1
+      readAddress(8'h00);
+
+      #1
+      write2Address(8'h00,8'h01);
+
+      #1
+      readAddress(8'h00);
+
+    
+      //Reset CRC32-Value
+      #1
+      write2Address(8'h01,8'h01);
+
+      #1
+      readAddress(8'h00);
 
     
       #1
-      read  = 0;    
-      writedata= 8'h01;
-      write=1;
-      address =8'h00;
+      write2Address(8'h00,8'h01);
 
       #1
-      write=0;
-      read  = 1;  
-      address =8'h00;
+      readAddress(8'h00);
 
-
-          #1
-      read  = 0;    
-      writedata= 8'h01;
-      write=1;
-      address =8'h00;
-
-      #1
-      write=0;
-      read  = 1;  
-      address =8'h00;
-
-       #1
-      read  = 0;    
-      writedata= 8'h01;
-      write=1;
-      address =8'h00;
-
-      #1
-      write=0;
-      read  = 1;  
-      address =8'h00;
-
-    
-   	   
+       
     end 
 
+  //Perform Hardware reset
+  task hwReset;
+     begin
+       $display("hwReset()");
+      clk   = 1; 
+      reset = 1;
+      #1
+      reset = 0; 
+      clk   = 0; 
+     end
+  endtask
   
-    always  
-      #1  clk =  ! clk; 
+  //Read data from a given address
+  task automatic readAddress;
+  	input byte _addr;  
+    begin
+      $display("readAddress() _addr: 0x%h", _addr);
+     #1
+     clk=1;
+     read  = 1;    
+     address =_addr;
 
+     #1
+     clk=0;
+     read  = 0;    
+
+    end
+  endtask
+
+  //Write data to a given address
+  task automatic write2Address;
+  	input byte _addr;  
+    input byte _value;    
+    begin
+      $display("write2Address() _addr: 0x%h _value: 0x%h", _addr,_value);
+     #1
+     clk=1;
+     read  = 0;    
+     writedata= _value;
+     write=1;
+     address =_addr;
+
+     #1
+     clk=0;
+     write=0;
+
+    end
+  endtask
   
   
+  //Displays signals on changes
   initial  begin
     $display("\t\ttime,\tclk,\treset,\tread\twrite\taddress\\twriteData\treaddatat"); 
     $monitor("%d,\t%b,\t%b,\t%b,\t%b,\t0x%h,\t0x%h,\t0x%h",$time, clk,reset,read,write,address,writedata,readdata); 
